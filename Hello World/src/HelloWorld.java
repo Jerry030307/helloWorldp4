7dbb097eaa8eb4f68ebb6b07e24db040b9b4eaad
1909973 <1909973@01-19831.pusd.dom>
2023-09-14 15:02:06 -0700

    hello world

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//sus amongus
+		//amogus sus
+		//jiinuiiu
+		//omg so much code i am pro hacker i hack the pentagon
+		System.out.print("hello world e  [stekghspoerigjpoiserjgsergpesriogjesporgjkpsoerjksgpejkrg 1+2=4");
+	}
+
+}
